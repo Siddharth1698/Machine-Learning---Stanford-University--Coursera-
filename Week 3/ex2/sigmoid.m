a61da6b6f7ee27187cd0e91501d1604c8d0af571
Siddharth M <siddharth.m98@gmail.com>
2019-06-03 12:28:59 +0530

    matlab assignment on week 3

--- Week 3/ex2/sigmoid.m
+++ Week 3/ex2/sigmoid.m
@@ -1,6 +1,6 @@
 function g = sigmoid(z)
-%SIGMOID Compute sigmoid function
-%   g = SIGMOID(z) computes the sigmoid of z.
+%SIGMOID Compute sigmoid functoon
+%   J = SIGMOID(z) computes the sigmoid of z.
 
 % You need to return the following variables correctly 
 g = zeros(size(z));
@@ -8,6 +8,12 @@ g = zeros(size(z));
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
+
+f=@(h) 1 ./ (1 + exp(-h)); % 1 / (1 + exp(-h));
+
+
+g = f(z);
+
 
 
 
